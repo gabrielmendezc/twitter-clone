b47145072f790bec32b42224143ea398cfabcf66
gabrielmendezc <43379549+gabrielmendezc@users.noreply.github.com>
2019-12-06 19:02:00 +0100

    setup apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from 'react-apollo'
+
+const GET_ME = gql`
+  {
+    me {
+      id
+      email
+      username
+    }
+  }
+`
 
 const App: React.FC = () => {
+  const { loading, error, data } = useQuery(GET_ME)
+
+  if (error) return <h1>Error</h1>
+  if (loading) return <h1>Loading...</h1>
+
+  console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +33,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
