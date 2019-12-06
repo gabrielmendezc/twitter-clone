dd267038fcb285bfa09dcfa32f68203dff3b7876
gabrielmendezc <43379549+gabrielmendezc@users.noreply.github.com>
2019-12-06 19:12:56 +0100

    setup apollo client's cache

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
+const cache = new InMemoryCache()
+
 const client = new ApolloClient({
-  uri: '/graphql'
+  uri: '/graphql',
+  cache
 })
 
 ReactDOM.render(
