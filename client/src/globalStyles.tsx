e776ff1fd1e33469143857697c89d0e8ac3edca8
gabrielmendezc <43379549+gabrielmendezc@users.noreply.github.com>
2019-12-07 22:07:05 +0100

    decided and set up color palette for the site

--- client/src/globalStyles.tsx
+++ client/src/globalStyles.tsx
@@ -10,9 +10,29 @@ export const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  :root {
+    --dominant-clr: #4670EC;
+    --complementary-1: #5025BA;
+    --complementary-2: #6A5F87;
+    --complementary-3: #EFC784;
+    --complementary-4: #BA6F25;
+  }
+
   body {
     margin: 0;
     font-family: 'Roboto', Arial, sans-serif;
   }
 `
-// Color palette:
+/* Color palette:
+  Color for highlighting *relevant* actions (dominant color): 
+  {
+    --dominant-clr: #4670EC;
+  }
+  Complementary colors to dominant: 
+  {
+    --complementary-1: #5025BA;
+    --complementary-2: #6A5F87;
+    --complementary-3: #EFC784;
+    --complementary-4: #BA6F25;
+  }
+*/
